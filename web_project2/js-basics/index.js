b14703fc91328d34d61c6b459d1d05e9bec0e33f
kitmattcheng04 <105215696+kitmattcheng04@users.noreply.github.com>
2022-05-09 20:31:13 +0800

    Add files via upload

---
+++ web_project2/js-basics/index.js
@@ -0,0 +1,17 @@
+let selectedcolors = ['red','green'];
+selectedcolors[2]='blue';
+console.log(selectedcolors.length);
+console.log(selectedcolors[2]);
+
+function greet(name, lastname) {
+  console.log('first ' + name + ' ' + lastname);
+}
+greet('John','Smith');
+
+function square(number){
+    return number*number
+}
+
+let number = square(2);
+console.log(number);
+
